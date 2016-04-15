b08216cb549541d46306b2a90690c7ef0d580e3b
Herman Chan <hermchan89@gmail.com>
2016-04-15 14:46:44 -0700

    GET completed and got back data

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,4 +4,15 @@ angular.module('libraryApp')
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+  vm.newBook = {};
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(function successCallback(response){
+    console.log('got back response', response);
+    vm.books = response.data;
+  }, function errorCallback(response){
+    console.log('error getting back data', response);
+  });
 }
