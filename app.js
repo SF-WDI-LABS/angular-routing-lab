8af72f35a5b2e5e47d50848909dc6b33f500cecd
Herman Chan <hermchan89@gmail.com>
2016-04-15 13:31:03 -0700

    added BooksShow/BooksIndex route on app.js

--- app.js
+++ app.js
@@ -10,13 +10,15 @@ config.$inject = ['$routeProvider', '$locationProvider'];
 function config (  $routeProvider,   $locationProvider  )  {
   $routeProvider
     .when('/', {
-      templateUrl: /* Include the path to the index template */,
-      controller:  /* Which controller do you want the main page to use */,
-      controllerAs:/* What will you call the controller in the html? */
+      templateUrl: '/templates/books/index.html',/* Include the path to the index template */
+      controller:  'BooksIndexController',/* Which controller do you want the main page to use */
+      controllerAs: 'booksIndexCtrl'/* What will you call the controller in the html? */
     })
     /* Include the additional route here! */
     .otherwise({
-      redirectTo: '/'
+      redirectTo: '/templates/books/show.html',
+      controller: 'BooksShowController',
+      controllerAs: 'booksShowCtrl'
     });
 
   // this just makes it so our URLs don't have /#/ in them.
@@ -25,4 +27,4 @@ function config (  $routeProvider,   $locationProvider  )  {
       enabled: true,
       requireBase: false
     });
-};
+}
