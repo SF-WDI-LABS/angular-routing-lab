d1bba5868f2eb059f6f743f192cad8a03b0991b3
chyktgw <chiyuki.x3@gmail.com>
2016-04-15 18:31:54 -0700

    Books List index complete

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,4 +4,17 @@ angular.module('libraryApp')
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(onSuccess, onError);
+  function onSuccess(response){
+    vm.books = response.data.books;
+    console.log(response.data.books);
+  }
+  function onError(error){
+    console.log('error founddd noooo');
+  }
+
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -4,4 +4,13 @@ angular.module('libraryApp')
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+  
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books/'+ $routeParams.id
+  }).then(onSuccess);
+  function onSuccess(response){
+    console.log('here\'s the data for book', bookId, ':', response.data);
+    vm.book = response.data;
+  }
+}
