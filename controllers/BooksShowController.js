287274c60bc2711d640bdf96a8198fe8ea080df9
Herman Chan <hermchan89@gmail.com>
2016-04-17 19:19:25 -0700

    got a specific book to render onto page

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,17 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+  .controller('BooksShowController', BooksShowController);
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books' + "/" + $routeParams.id,
+  }).then(function callBackSuccess(response) {
+    console.log('show success', response.data);
+    vm.book = response.data;
+  }, function callBackError(err) {
+    console.log('Error getting response data', err);
+  });
+}
