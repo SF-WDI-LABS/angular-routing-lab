1a2242ba101d4650082b8dbe2b523ef43573880b
klawton1 <klawton1@gmail.com>
2017-02-25 18:39:11 -0800

    moved edit form to specific album page

--- public/scripts/controllers/booksController.js
+++ public/scripts/controllers/booksController.js
@@ -7,29 +7,6 @@ function BooksController(   $http,   $routeParams) {
  var vm = this;
  console.log("BOOK controller!") 
  vm.books = [];
- vm.editBook = {
-  title: "",
-  image: "",
-  author: "",
-  releaseDate: "",
- }
- vm.makeEdit = function(book){
-  for(key in vm.editBook){
-    vm.editBook[key] = book[key]
-  }
- }
- vm.submit = function(book){
-  var index = vm.books.indexOf(book);
-  console.log(index);
-  $http({
-    method: "PUT",
-    url: "https://super-crud.herokuapp.com/books/" + book._id,
-    data: vm.editBook
-  }).then(function(res){
-    console.log("success!");
-    vm.books[index] = res.data;
-  })
- }
  $http({
   method: "GET",
   url: "https://super-crud.herokuapp.com/books",
--- public/scripts/controllers/oneBookController.js
+++ public/scripts/controllers/oneBookController.js
@@ -6,7 +6,29 @@ OneBookController.$inject = ['$http', '$routeParams'];
 function OneBookController(    $http,   $routeParams) {
   var vm = this;
   vm.book = {};
-  console.log("found One Book!");
+  vm.editBook = {
+    title: "",
+    image: "",
+    author: "",
+    releaseDate: "",
+  }
+  vm.makeCopy = function(book){
+    for(key in vm.editBook){
+      vm.editBook[key] = book[key]
+    }
+    console.log(vm.editBook);
+  }
+  vm.submit = function(book){
+    $http({
+      method: "PUT",
+      url: "https://super-crud.herokuapp.com/books/" + book._id,
+      data: vm.editBook
+    }).then(function(res){
+      console.log("success!");
+      vm.book = res.data;
+    })
+  }
+  console.log("found A Book!");
   $http({
     method: "GET",
     url: "https://super-crud.herokuapp.com/books/" + $routeParams.id,
